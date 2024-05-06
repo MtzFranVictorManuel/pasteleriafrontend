131cb2e50af5c6c3321442f485d50392e3451962
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-05-06 14:43:16 -0600

    actualizacion

--- pasteleriafront/src/main.jsx
+++ pasteleriafront/src/main.jsx
@@ -1,25 +1,39 @@
+import { useState, useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Login from "./pages/Login.jsx"; 
 import "./styles/index.css";
 import Productos from "./pages/Productos.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AgregarProducto from "./pages/agregarProducto/AgregarProducto.jsx";
 import Contexto from "./pages/agregarProducto/Contexto.jsx";
 import Registrar from "./pages/Registrar.jsx";
+import EditarPerfil from "./pages/usuario/EditarPerfil.jsx";
+import PrivateRoute from "./pages/rutasPrivadas/PrivateRoute.jsx";
+import { AuthContext, AuthProvider } from "./pages/autentificador/AuthContext.jsx";
+import Home from "./pages/usuario/Home.jsx";
+
 
 
 function Main() {
+  const { isLoggedIn } = useContext(AuthContext);
+  console.log("isLoggedIn:", isLoggedIn);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/productos" element={<Productos />} />
         <Route path="/agregarProducto" element={<Contexto><AgregarProducto /></Contexto>} />
         <Route path="/registrar" element={<Registrar />} />
-
+        <Route path="/editarPerfil" element={<EditarPerfil />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <Main />
+  </AuthProvider>
+);
---
+++ pasteleriafront/src/pages/autentificador/AuthContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        console.log("Token from localStorage:", token);
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
