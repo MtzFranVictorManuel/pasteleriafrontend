4861aac5d93ddde236a7e4bfcb9688bffdf3d357
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-05-27 18:54:09 -0600

    actualizacion

--- pasteleriafront/src/pages/HistorialPedidos.jsx
+++ pasteleriafront/src/pages/HistorialPedidos.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
+import { Chip } from '@mui/material';
 
 const HistorialPedidos = () => {
   const [historial, setHistorial] = useState([]);
@@ -34,14 +35,14 @@ const HistorialPedidos = () => {
       <Table>
         <TableHead>
           <TableRow>
-            {rol === '2' && <TableCell>Nombre Cliente</TableCell>}
-            <TableCell>Código de Pedido</TableCell>
-            <TableCell>Estado</TableCell>
-            <TableCell>Fecha de Pedido</TableCell>
-            <TableCell>Fecha de Entrega</TableCell>
-            <TableCell>Calle</TableCell>
-            <TableCell>Colonia</TableCell>
-            {rol === '2' && <TableCell>Detalles</TableCell>}{/* Agregamos una nueva columna para mostrar los detalles del pedido */}
+            {rol === '2' && <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Nombre Cliente</TableCell>}
+            <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Código de Pedido</TableCell>
+            <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Estado</TableCell>
+            <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Fecha de Pedido</TableCell>
+            <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Fecha de Entrega</TableCell>
+            <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Calle</TableCell>
+            <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Colonia</TableCell>
+            {rol === '2' && <TableCell style={{ backgroundColor: '#f5f5f5', fontWeight: 'bold' }}>Detalles</TableCell>}
           </TableRow>
         </TableHead>
         <TableBody>
@@ -49,14 +50,35 @@ const HistorialPedidos = () => {
             <TableRow key={pedido.codigoPedido}>
               {rol === '2' && <TableCell>{pedido.nombreUsuario}</TableCell>}
               <TableCell>{pedido.codigoPedido}</TableCell>
-              <TableCell>{pedido.estado}</TableCell>
+              <TableCell>
+                {(() => {
+                  switch (pedido.estado) {
+                    case 'pagado':
+                      return <Chip label="Pagado" style={{ backgroundColor: '#1C7C19', color: 'white' }} />;
+                    case 'pendiente pago':
+                      return <Chip label="Pendiente Pago" style={{ backgroundColor: '#B4D33F', color: 'white' }} />;
+                    case 'pedido aceptado':
+                      return <Chip label="Pedido Aceptado" style={{ backgroundColor: '#3FB4D3', color: 'white' }} />;
+                    case 'en proceso':
+                      return <Chip label="En Proceso" style={{ backgroundColor: '#EA8E17', color: 'white' }} />;
+                    case 'listo para entregar':
+                      return <Chip label="Listo Para Entregar" style={{ backgroundColor: '#1731EA', color: 'white' }} />;
+                    case 'entregado':
+                      return <Chip label="Entregado" style={{ backgroundColor: '#6AA264', color: 'white' }} />;
+                    case 'cancelado':
+                      return <Chip label="Cancelado" style={{ backgroundColor: '#F63333', color: 'white' }} />;
+                    default:
+                      return null;
+                  }
+                })()}
+              </TableCell>
               <TableCell>{pedido.fechaPedido}</TableCell>
               <TableCell>{pedido.fechaEntrega}</TableCell>
               <TableCell>{pedido.calle}</TableCell>
               <TableCell>{pedido.colonia}</TableCell>
               {rol === '2' &&
                 <TableCell>
-                  <Button style={{ backgroundColor: '#CD006A', color:'white' }} href={`/historialPedidos/${pedido.codigoPedido}`}>Ver Detalles</Button>
+                  <Button style={{ backgroundColor: '#CD006A', color: 'white' }} href={`/historialPedidos/${pedido.codigoPedido}`}>Ver Detalles</Button>
                 </TableCell>
               }
             </TableRow>
